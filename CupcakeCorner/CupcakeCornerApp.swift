288a4f738122e4ead4592ee544a1f54de8d3ac1c
your_name <tofuyong@gmail.com>
2023-09-03 13:28:48 +0800

    first commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Andrea Yong on 2/9/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
